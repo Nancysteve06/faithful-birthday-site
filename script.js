17981da744ab89391af6f5dfa41c863f9e00e27e
Nancysteve06 <ballynallly@gmail.com>
2025-06-30 18:47:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-function makeWish() {
-  const wish = document.getElementById("wishInput").value.trim();
-  if (wish) {
-    alert("🌟 Your wish has been sent to the stars, Fella says thanks! ✨\n\n" + wish);
-    document.body.style.background = "#ffe6f9";
+function makeWish(){
+  const wish = document.getElementById('wishInput').value.trim();
+  if(wish){
+    alert("🌟 Your wish reached the stars, Fella says thanks! ✨\n" + wish);
+    explodeFairyDust();
   } else {
-    alert("Hey now, don’t forget to type your wish 💖");
+    alert("Please type your magical wish 💖");
   }
 }
+
+function explodeFairyDust(){
+  const d = document.createElement('div');
+  d.classList.add('fairy-explode');
+  document.querySelector('.card').appendChild(d);
+  setTimeout(()=>d.remove(),2000);
+}
+
+// Fairy dust explosion
+const style = document.createElement('style');
+style.innerHTML = `
+.fairy-explode {
+  position:absolute; top:50%; left:50%;
+  width:200px; height:200px;
+  background: url('https://i.imgur.com/OJfS0vX.png') no-repeat;
+  background-size:contain;
+  animation: popDust 1s ease-out;
+}
+@keyframes popDust {
+  0% { transform: translate(-50%,-50%) scale(0); opacity:1 }
+  100% { transform: translate(-50%,-50%) scale(1.5); opacity:0 }
+}`;
+document.head.appendChild(style);
